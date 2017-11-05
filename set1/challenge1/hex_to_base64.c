8d41fd1a7a3eef6a321cbbaabd329807c237cc5c
manila <manila@users.noreply.github.com>
2017-11-05 15:33:29 -0700

    Finished encode_block function

--- set1/challenge1/hex_to_base64.c
+++ set1/challenge1/hex_to_base64.c
@@ -11,22 +11,22 @@ void	encode_block(unsigned char *in, unsigned char *out);
 int	main(int argc, char **argv)
 {
 	int		i;
-	unsigned char out[4];
-	unsigned char man[3] = {77, 97, 110};
+	unsigned char	out[4];
+	unsigned char	man[4] = {77};
 	
 	encode_block(man, out);
 
-	printf("%d", out[0]);
+	printf("%s", out);
 	
 	return (0);	
 }
 
 void	encode_block(unsigned char *in, unsigned char *out)
 {
-	out[0] = in[0] >> 2;
-	out[1] = ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4);
-	out[2] = ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6);
-	out[3] = (in[2] & 0x03);
+	out[0] = (unsigned char) b64chars[in[0] >> 2];
+	out[1] = (unsigned char) b64chars[((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4)];
+	out[2] = (unsigned char) ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) > 0 ? b64chars[((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6)] : '=';
+	out[3] = (unsigned char) (in[2] & 0x3f) > 0 ? b64chars[(in[2] & 0x3f)] : '=';
 }
 
 int	index_of(char *arr, char c)
@@ -43,7 +43,7 @@ int	index_of(char *arr, char c)
 	return (0);
 }
 
-int	htoi(char *hex_pair)
+unsigned char	hex_pair_to_dec(char *hex_pair)
 {
 	int	num;
 
@@ -51,6 +51,19 @@ int	htoi(char *hex_pair)
 	num = num << 4;
 	num += index_of(b16chars, *(++hex_pair)); 
 
-	printf("\n%d\n", num);
 	return (num);
+}
+
+char	*hex_to_base64(char *hexstr)
+{
+	size_t		i;
+	unsigned char	*base64str;
+
+	i = 0;
+	base64str = (unsigned char *) malloc(sizeof(unsigned char) * (4 * (strlen(hexstr) / 3)) + 1);
+	while (hexstr[i] && hexstr[i + 1])
+	{	
+		i += 2;
+	}
+	return (base64str);
 }
