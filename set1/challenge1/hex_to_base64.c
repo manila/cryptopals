30d8bbb2b9a65ea09a773fd026e6a6d1ae24f0d8
manila <manila@users.noreply.github.com>
2017-11-05 00:55:39 -0600

    Added encode_block function

--- set1/challenge1/hex_to_base64.c
+++ set1/challenge1/hex_to_base64.c
@@ -3,13 +3,30 @@
 #include <string.h>
 
 char b16chars[] = "0123456789ABCDEF";
+char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-void	htoi(char *hex_pair);
+int	htoi(char *hex_pair);
+void	encode_block(unsigned char *in, unsigned char *out);
 
 int	main(int argc, char **argv)
 {
-	htoi("4F");
+	int		i;
+	unsigned char out[4];
+	unsigned char man[3] = {77, 97, 110};
+	
+	encode_block(man, out);
+
+	printf("%d", out[0]);
+	
 	return (0);	
+}
+
+void	encode_block(unsigned char *in, unsigned char *out)
+{
+	out[0] = in[0] >> 2;
+	out[1] = ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4);
+	out[2] = ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6);
+	out[3] = (in[2] & 0x03);
 }
 
 int	index_of(char *arr, char c)
@@ -23,19 +40,17 @@ int	index_of(char *arr, char c)
 			return (i);
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
 
-void	htoi(char *hex_pair)
+int	htoi(char *hex_pair)
 {
-	int num;
-	
-	num = 0;
-	while (*hex_pair)
-	{
-		num = num << 4;
-		num += index_of(b16chars, *hex_pair);
-		hex_pair++;
-	}
+	int	num;
+
+	num = index_of(b16chars, *hex_pair);
+	num = num << 4;
+	num += index_of(b16chars, *(++hex_pair)); 
+
 	printf("\n%d\n", num);
+	return (num);
 }
