5700d639bd5be6ef54cc49ed68622a552074fede
manila <manila@users.noreply.github.com>
2017-11-05 18:08:20 -0700

    Finished HEX pair function

--- set1/challenge1/hex_to_base64.c
+++ set1/challenge1/hex_to_base64.c
@@ -5,33 +5,21 @@
 char b16chars[] = "0123456789ABCDEF";
 char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-int	htoi(char *hex_pair);
-void	encode_block(unsigned char *in, unsigned char *out);
-
-int	main(int argc, char **argv)
+unsigned char	*Byte3asChar4(unsigned char *in)
 {
-	int		i;
-	unsigned char	out[4];
-	unsigned char	man[4] = {77};
-	
-	encode_block(man, out);
+	unsigned char out[4];
 
-	printf("%s", out);
-	
-	return (0);	
-}
-
-void	encode_block(unsigned char *in, unsigned char *out)
-{
 	out[0] = (unsigned char) b64chars[in[0] >> 2];
 	out[1] = (unsigned char) b64chars[((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4)];
 	out[2] = (unsigned char) ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) > 0 ? b64chars[((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6)] : '=';
 	out[3] = (unsigned char) (in[2] & 0x3f) > 0 ? b64chars[(in[2] & 0x3f)] : '=';
+
+	return (out);
 }
 
-int	index_of(char *arr, char c)
+size_t		index_of(char *arr, char c)
 {
-	int i;
+	size_t i;
 
 	i = 0;
 	while (arr[1])
@@ -43,7 +31,7 @@ int	index_of(char *arr, char c)
 	return (0);
 }
 
-unsigned char	hex_pair_to_dec(char *hex_pair)
+unsigned char	Base16PairToInt(char *hex_pair)
 {
 	int	num;
 
@@ -54,7 +42,7 @@ unsigned char	hex_pair_to_dec(char *hex_pair)
 	return (num);
 }
 
-char	*hex_to_base64(char *hexstr)
+char		*Base16toBase64(char *hexstr)
 {
 	size_t		i;
 	unsigned char	*base64str;
@@ -63,7 +51,14 @@ char	*hex_to_base64(char *hexstr)
 	base64str = (unsigned char *) malloc(sizeof(unsigned char) * (4 * (strlen(hexstr) / 3)) + 1);
 	while (hexstr[i] && hexstr[i + 1])
 	{	
+		printf("%d", Base16PairToInt(&hexstr[i]));
 		i += 2;
 	}
 	return (base64str);
+}
+
+int		main(int argc, char **argv)
+{
+	Base16toBase64("4D616E");	
+	return (0);	
 }
