3dd0519a10524c1268b1d5d697afcafa2f868b54
manila <manila@users.noreply.github.com>
2017-11-10 23:00:20 -0700

    Updated index_of function

--- set1/challenge1/hex_to_base64.c
+++ set1/challenge1/hex_to_base64.c
@@ -20,23 +20,14 @@ void	*Char2asByte1(unsigned char *in, unsigned char *out)
 
 unsigned char	index_of(char *arr, char c)
 {
-	int i;
-
-	i = 0;
-	while (arr[1])
-	{
-		if (arr[i] == c)
-			return ((unsigned char) i);
-		i++;
-	}
-	return (0);
+	return (strchr(arr, c) - strchr(arr, arr[0]));
 }
 
 unsigned char	Base16PairToChar(char *hex_pair)
 {
 	unsigned char	out;
 	unsigned char	in[2];
-	
+
 	in[0] = index_of(b16chars, hex_pair[0]);
 	in[1] = index_of(b16chars, hex_pair[1]);
 
@@ -63,14 +54,14 @@ char		*Base16toBase64(char *hexstr)
 
 	while (hexstr[i] && hexstr[i + 1])
 	{	
-		printf("%d", Base16PairToChar(&hexstr[i]));
+		printf("%c", Base16PairToChar(&hexstr[i]));
 		i += 2;
 	}
 	return (base64str);
 }
 
 
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	Base16toBase64("4D616E");	
 	return (0);	
